140e1c8a3ca42d9c1d46802193a848910569894f
Brevin <bcoday974@lsr7.net>
2018-12-12 09:03:58 -0600

    add pong.py

---
+++ pong.py
@@ -0,0 +1,17 @@
+# Functions -------------------------
+from sense_hat import SenseHat
+sense = SenseHat()
+
+white = (255, 255, 255)
+
+bat_y = 4
+
+def draw_bat():
+    sense.set_pixel(0, bay_y, white)
+    
+bat_y + 1
+bat_y - 1
+
+# Main program ------------------------
+
+                
